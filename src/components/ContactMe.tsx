8444ef4ed52abb304f2f734a0089bfe7903e054b
zackymods <zackymods@gmail.com>
2024-08-04 18:45:57 -0300

    Update ContactMe.tsx

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -98,50 +98,7 @@ export default function ContactMe() {
 				</span>
 			</div>
 
-			<form
-				className="flex w-full flex-col px-10 py-5 pt-10 sm:w-1/2 sm:px-0 sm:pl-10"
-				onSubmit={submitForm}
-				ref={form}
-			>
-				<input
-					type="text"
-					placeholder="Your Name"
-					name="from_name"
-					required
-					className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
-				/>
-				<input
-					type="email"
-					placeholder="Your Email"
-					name="from_email"
-					required
-					className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
-				/>
-				<textarea
-					rows={5}
-					placeholder="Your Message"
-					name="message"
-					minLength={10}
-					required
-					className="max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
-				/>
-				<button
-					type="submit"
-					disabled={disable}
-					className={`btn group relative mt-5 inline-flex h-12 w-40 items-center justify-start overflow-hidden rounded-lg border-2 bg-white text-sm transition-all dark:border-0 dark:bg-gray-900`}
-				>
-					<span
-						className={`-z-1 absolute left-0 top-0 h-0 w-0 rounded ${
-							status === 'Sent!' ? 'bg-green-500 text-black' : 'bg-cyan-600'
-						} transition-all duration-500 ease-out group-hover:h-full group-hover:w-full ${
-							status === 'Sent!' ? 'text-white dark:bg-green-500' : 'dark:bg-cyan-700'
-						}`}
-					></span>{' '}
-					<span className="z-10 w-full text-gray-400 transition-colors duration-300 ease-in-out group-hover:text-white">
-						{status}
-					</span>
-				</button>
-			</form>
+			
 		</div>
 	);
 }
